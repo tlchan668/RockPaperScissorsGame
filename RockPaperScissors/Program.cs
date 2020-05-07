5c138d7ab1710aafb1e8b6f9d60f513b3f7b22bf
Traci Chan <tlc668@gmail.com>
2020-05-07 07:47:28 -0400

    Add project files.

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks.Sources;
+
+namespace RockPaperScissors {
+    class Program {
+        static void Main(string[] args) {
+
+            string inputPlayer;
+            string inputCPU;
+            int randomInt;
+
+            int scoreCPU = 0; int scorePlayer = 0;
+            Boolean play = true;
+
+            while(play) {
+                Console.WriteLine("Rock Paper Scissors!");
+                Console.WriteLine("Which do you choose? ");
+                inputPlayer = Console.ReadLine();
+                //Console.WriteLine(inputPlayer);
+
+                Random rnd = new Random();
+
+                randomInt = rnd.Next(1, 3);
+                //Console.WriteLine(randomInt);
+                switch(randomInt) {
+                    case 1:
+                        inputCPU = "ROCK";
+                        Console.WriteLine("CPU choose rock");
+                        if(inputPlayer == "scissors") {
+                            Console.WriteLine("CPU wins!");
+                            scoreCPU++;
+                        }
+                        else {
+                            Console.WriteLine("YOU win!");
+                            scorePlayer++;
+                        }
+
+                        break;
+                    case 2:
+                        inputCPU = "PAPER";
+                        Console.WriteLine("CPU choose paper");
+                        break;
+                    case 3:
+                        inputCPU = "SCISSORS";
+                        Console.WriteLine("CPU choose scissors");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid entry!");
+                        break;
+
+                }
+
+                Console.WriteLine("Play again (y or n)?");
+                inputPlayer = Console.ReadLine();
+                if(inputPlayer == "n") {
+                    Console.WriteLine("You won " + scorePlayer);
+                    Console.WriteLine("CPU won " + scoreCPU);
+                    Console.WriteLine("Thanks for playing!");
+                    break;
+
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
